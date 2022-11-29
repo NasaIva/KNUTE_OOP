44c1ddcb1e1b2bb0d5258831701897b383acc5c3
NasaIva <106171804+NasaIva@users.noreply.github.com>
2022-11-29 10:04:41 +0200

    Lab_2_3

---
+++ Lab2_3/Lab2_3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lab2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee("Anastasiia", "Ivanchenko", "Director", 1);
+            person.Wage();
+        }
+    }
+
+    class Employee
+    {
+        private string name { get; set; }
+        private string surname { get; set; }
+        private string spot { get; set; }
+        private int experience { get; set; }
+        private double wage { get; set; }
+
+
+        public Employee(string name, string surname, string spot, int experience)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.spot = spot;
+            this.experience = experience;
+        }
+
+        public void Wage()
+        {
+            double wage0 = 10000;
+            double tax = 0.9;
+
+            if ("Trainee" == spot)
+            {
+                wage = wage0 * 1 * experience * tax;
+                Console.WriteLine($"Name: {name} \nSurname: {surname} " +
+                                  $"\nSpot: {spot} \nWage: {wage} \nTax fee 10%");
+            }
+            else if ("Employee" == spot)
+            {
+                wage = wage0 * 2 * experience * tax;
+                Console.WriteLine($"Name: {name} \nSurname: {surname} " +
+                                  $"\nSpot: {spot} \nWage: {wage} \nTax fee 10%");
+            }
+            else if ("Manager" == spot)
+            {
+                wage = wage0 * 3 * experience * tax;
+                Console.WriteLine($"Name: {name} \nSurname: {surname} " +
+                                  $"\nSpot: {spot} \nWage: {wage} \nTax fee 10%");
+            }
+            else
+            {
+                Console.WriteLine("Error: This position is not listed");
+            }
+        }
+    }
+}

9a6587cb6d7b313782d350d0522cd74242fdc7f6
NasaIva <106171804+NasaIva@users.noreply.github.com>
2022-12-06 16:12:15 +0200

    Lab3_1

---
+++ Lab3_1/Lab3.1/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double side1, side2;
+            Console.Write("Side 1: ");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Side 2: ");
+            side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(side1, side2);
+            rectangle.PerimeterCalculator();
+            rectangle.AreaCalculator();
+            Console.WriteLine($"Perimeter = {rectangle.Perimeter}");
+            Console.WriteLine($"Area = {rectangle.Area}");
+            Console.ReadLine();
+        }
+    }
+}

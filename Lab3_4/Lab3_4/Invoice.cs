012a7d9469639fbe48133d36564f4c6466de5a67
NasaIva <106171804+NasaIva@users.noreply.github.com>
2022-12-06 16:15:00 +0200

    Lab3_4

---
+++ Lab3_4/Lab3_4/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab3_4
+{
+    class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+        private string article;
+        private int quantity;
+        private double PDW = 0.2;
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        public double CalculateValueOrder()
+        {
+            int choose = 0;
+            Console.Write("0 without taxes, 1 with taxes:");
+            choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 0:
+                    return account;
+                case 1:
+                    return (account * PDW) + account;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Lab3_4/Lab3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(666, "Anastasiia", "PM");
+            Console.WriteLine(invoice.CalculateValueOrder());
+        }
+    }
+}

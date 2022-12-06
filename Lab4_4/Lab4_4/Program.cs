c16cf0ecf64f0f0d481d6ae68fa8bc471f87adb7
NasaIva <106171804+NasaIva@users.noreply.github.com>
2022-12-06 16:02:22 +0200

    Lab4_4

---
+++ Lab4_4/Lab4_4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "";
+            ApplicationLicense applicationLicense;
+            Console.WriteLine("Введіть ключ пробного режиму чи платного: ");
+            applicationLicense = new ApplicationLicense(Console.ReadLine());
+        }
+    }
+}

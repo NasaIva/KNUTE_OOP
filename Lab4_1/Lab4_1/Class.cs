cddd0ad18eb898abb9375812db722adc3350d228
NasaIva <106171804+NasaIva@users.noreply.github.com>
2022-12-06 15:59:59 +0200

    Lab4_1

---
+++ Lab4_1/Lab4_1/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab4_1;
+
+public class Class: Program
+{
+
+    internal override void WriteInDebug(string stringParam)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Param of type " + stringParam.GetType());
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Param of type " + intParam.GetType());
+    }
+}
---
+++ Lab4_1/Lab4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab4_1;
+
+public class Program
+{
+
+    virtual internal void WriteinDebug(string stringParam, int intParam)
+    {
+        Debug.WriteLine(message: "String parameter: " + stringParam);
+        Debug.WriteLine(message: "Int parameter: " + intParam);
+
+    }
+
+}
+
